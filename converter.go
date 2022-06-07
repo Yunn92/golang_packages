97e71153aaa2e573fc30976f0c6356ca443268ec
Yunn92 <Yunn92@ya.ru>
2022-06-07 14:43:34 +0500

    v1. It's finally woring

--- converter.go
+++ converter.go
@@ -1,9 +1,19 @@
 package main
 import (
-	"C:/Users/ishir/OneDrive/Documents/golang/first_program/tempconv"
+	"tempconv"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main(){
-	fmt.Println(tempconv.AbsoluteZeroC)
+	for _, arg := range os.Args[1:]{
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil{
+			fmt.Fprintf(os.Stderr, "cfk: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Farenheit(t)
+		fmt.Printf("%s = %s\n%s = %s\n%s = %s\n\n", f, tempconv.FToC(f), f, tempconv.FToK(f), tempconv.FToK(f), tempconv.FToC(f))
+	}
 }
--- tempconv/conv_func.go
+++ tempconv/conv_func.go
@@ -1,18 +1,18 @@
 package tempconv
 
-func CToF(c Celsius){
+func CToF(c Celsius) Farenheit{
 	return Farenheit(c*9/5+32)
 }
-func CToK(c Celsius){
+func CToK(c Celsius) Kelvin{
 	return Kelvin(c - 273.15)
 }
-func KToC(k Kelvin){
+func KToC(k Kelvin) Celsius{
 	return Celsius(k + 273.15)
 }
-func KToF(k Kelvin){
+func KToF(k Kelvin) Farenheit{
 	return Farenheit(k*1.8 - 459.67)
 }
-func FToK(f Farenheit){
+func FToK(f Farenheit) Kelvin{
 	return Kelvin((f+459.67)/1.8)
 }
 func FToC(f Farenheit) Celsius{
